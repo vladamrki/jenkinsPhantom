30e097d13547507b58d9c847390cd29f7f210db1
vladamrki <vladimirmrksic2013@gmail.com>
2016-02-11 10:02:10 +0100

    initial commit5

---
+++ src/test/java/Banking/test2.java
@@ -0,0 +1,33 @@
+package Banking;
+
+import org.testng.annotations.Test;
+
+import net.anthavio.phanbedder.Phanbedder;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+ 
+public class test2 {
+  private RemoteWebDriver driver;
+
+@Test
+  public void testSearchReturnsResults() throws MalformedURLException {
+    //Create instance of PhantomJS driver
+
+	 File phantomjs = Phanbedder.unpack();
+     DesiredCapabilities dcaps = new DesiredCapabilities();
+     dcaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, phantomjs.getAbsolutePath());
+     PhantomJSDriver driver = new PhantomJSDriver(dcaps);
+ 
+    //Navigate to the pageasd
+     driver.get("https://www.google.com");
+    
+  }
+}
