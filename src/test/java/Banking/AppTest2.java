9591d53ea25f29b187e58799e2ba0b6849620f01
vladamrki <vladimirmrksic2013@gmail.com>
2016-02-11 10:06:51 +0100

    initial commit6

--- src/test/java/Banking/AppTest2.java
+++ src/test/java/Banking/AppTest2.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
  
-public class test2 {
+public class AppTest2 {
   private RemoteWebDriver driver;
 
 @Test
